d5f8e0e9112ed68714a09661712cb5d7279f2ab6
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2024-03-02 22:12:00 +0700

    fix

--- server/src/main/java/com/example/server/DTO/MessageDTO.java
+++ server/src/main/java/com/example/server/DTO/MessageDTO.java
@@ -1,31 +1,25 @@
-package com.example.server.models;
+package com.example.server.DTO;
 
-
+import com.example.server.models.ChatRoom;
+import com.example.server.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
-import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
 
 import java.util.Date;
 
-@Node("message")
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class Message {
-    @Id
-    @GeneratedValue(UUIDStringGenerator.class)
+public class MessageDTO {
     private String id;
-    @Relationship(type = "SENT_MESSAGE", direction = Relationship.Direction.INCOMING)
-    private User sender;
-    @Relationship(type = "RECIPIENT_MESSAGE", direction = Relationship.Direction.INCOMING)
-    private ChatRoom room;
+    private String senderId;
+    private String roomId;
     private String content;
-    private Date timestamp;
 }
