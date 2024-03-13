8512dd15a908757860567538edd32a0039b600d7
chouchou <chaudiemhoang2@gmail.com>
2024-03-13 21:56:50 +0700

    #

--- server/src/main/java/com/example/server/configuration/JwtAuthenticationFilter.java
+++ server/src/main/java/com/example/server/configuration/JwtAuthenticationFilter.java
@@ -14,6 +14,8 @@ import java.security.Security;
 import jakarta.transaction.TransactionScoped;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,7 +27,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
+    @Autowired
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
     private final TokenRepository tokenRepository;
