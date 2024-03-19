76d2e7b4ff49dc7b0dab5d92b4877e71335c0a38
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2024-03-19 13:11:53 +0700

    Remove file serving functionality from PostController.

--- server/src/main/java/com/example/server/controllers/PostController.java
+++ server/src/main/java/com/example/server/controllers/PostController.java
@@ -30,32 +30,6 @@ public class PostController {
     private final PostService service;
     private final PostAttachmentService postAttachmentService;
 
-    @GetMapping("/attachment/img/{url}")
-    public ResponseEntity<Object> serveFile(@PathVariable String url) {
-        try {
-            // Đường dẫn đến thư mục chứa các tệp
-            String directoryPath = "src/main/resources/static/attachments/";
-
-            // Tạo một đối tượng Resource cho tệp cụ thể
-            Resource resource = new UrlResource(Paths.get(directoryPath+url).toUri());
-            System.out.println(resource.getURI());
-            // Kiểm tra xem tệp có tồn tại không
-            if (resource.exists() && resource.isReadable()) {
-                // Trả về ResponseEntity chứa tệp và các HTTP headers
-                return ResponseEntity.ok()
-                        .contentType(MediaType.IMAGE_PNG)
-                        .header("Content-Disposition", "attachment; filename=" + url)
-                        .body(resource);
-            } else {
-                // Trả về 404 Not Found nếu tệp không tồn tại
-                return Respond.fail(404,"E002","Not found");
-            }
-        }
-        catch (Exception e){
-            return Respond.fail(500,"E001",e.getStackTrace());
-        }
-    }
-
     @PostMapping("/create")
     public ResponseEntity<Object> post(@RequestBody List<PostAttachment> attachments, @RequestParam String content,@AuthenticationPrincipal Account account) {
         try {
