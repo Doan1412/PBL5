b12247a9637bfbe2c08fcb446e7c4d3356cff3c4
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2024-04-25 22:15:31 +0700

    PostController, PostRepository, and PostService to support adding and deleting comments with attachments"  The changes include:  * Adding a new field `attachment` to `CommentDTO` to support attach

--- server/src/main/java/com/example/server/DTO/CommentDTO.java
+++ server/src/main/java/com/example/server/DTO/CommentDTO.java
@@ -1,9 +1,13 @@
 package com.example.server.DTO;
 
+import com.example.server.models.Entity.PostAttachment;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -13,4 +17,5 @@ public class CommentDTO {
     private String content;
     private String postId;
     private int like_count;
+    private List<PostAttachment> attachment;
 }
